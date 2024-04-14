1b14048ceb79f60f099a359855aff6be76a85209
Arushisharma1610 <arushisharma2003bly@gmail.com>
2024-04-14 17:45:16 +0530

    Update App.js

--- dummy/src/App.js
+++ dummy/src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
 
 function App() {
+
+  const[text,setText] = useState(' ');
+  const[name, setName] = useState('hello');
+
+
+  //VARIATION - 1 On every render
+        // useEffect ( () => {
+        //   console.log("UI RENDERING IS DONE");
+        // })
+
+
+  //VARIATION - 2 On first render
+        // useEffect(() => {
+        //   console.log("UI RENDERING IS DONE");
+        // }, [])
+
+
+
+  // VARTATION - 3 first render +  whenever dependency changes
+        // useEffect ( () => {
+        //   console.log("Change in Dependency")
+        // }, [name])
+
+
+
+  // VARIATION -4 To Handel unmounting of a component
+        // useEffect( () => {
+        //   // event listerner added
+        //     console.log("Listner is added");
+
+        //   //listner removed
+        //     return () =>{
+        //       console.log("Listner is removed");
+        //     }
+        // }, [text])
+
+
+  function changeHandler (event) {
+      console.log(text);
+      setText(event.target.value);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="text" onChange={changeHandler}></input>
     </div>
   );
 }
